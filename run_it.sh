0b55d800a9d696b711b45438f1cc83af66c42c01
nruzic45 <n.ruzic45@protonmail.com>
2025-11-12 16:29:29 +0100

    document: modified the tutorials README, to change to the vgn directory

--- run_it.sh
+++ run_it.sh
@@ -3,6 +3,8 @@ roslaunch panda_moveit_config franka_control.launch robot_ip:=<robot_ip> load_gr
 
 roslaunch vgn panda_grasp.launch
 
+cd ~/catkin_ws/src/VGN_rb_demo/vgn
+
 python3 scripts/panda_grasp_ETF.py --model data/models/vgn_conv.pth
 
 
